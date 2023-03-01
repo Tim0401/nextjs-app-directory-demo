303111289bd5df30a2d2c7d25ee8ee7980fe2ac0
Tim0401 <36380433+Tim0401@users.noreply.github.com>
2023-03-01 19:21:19 +0000

    prisma db.tsの追加

---
+++ globals/db.ts
@@ -0,0 +1,13 @@
+// https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+import { PrismaClient } from '@prisma/client';
+import "server-only";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
