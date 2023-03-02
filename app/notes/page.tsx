963469feb4ce8d03fce9eef4b660002d5ee5c0e4
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2023-03-02 03:58:13 +0000

    ノート作成ページへのリンク追加

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -18,7 +18,7 @@ export default async function Page() {
   const notes = await getNotes();
   return (
     <main className="mx-2 sm:mx-4 relative">
-      <Link href="/notes" className="absolute top-0 right-2 z-10 text-white bg-pink-500 hover:bg-pink-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2">
+      <Link href="/notes/new" className="absolute top-0 right-2 z-10 text-white bg-pink-500 hover:bg-pink-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2">
         <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="4 4 8 8" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" clipRule="evenodd"></path></svg>
         <span className="sr-only">New Note</span>
       </Link>
