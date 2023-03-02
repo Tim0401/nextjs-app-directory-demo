d60ecc1afb6529946e80929b7b35eeaecfb6a95f
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2023-03-02 03:38:07 +0000

    ノートの追加APIの作成

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,9 +1,19 @@
+import { zUpsertNote } from "@/app/notes/type";
 import { prisma } from "@/globals/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const notes = await prisma.note.findMany();
   return NextResponse.json(notes)
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+  return new NextResponse(`${note.id}`, { status: 201 })
 }
