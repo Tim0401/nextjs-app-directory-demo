49db13b422b362a5a99e429ffe7443dfe9b0dd2f
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2023-03-02 04:02:25 +0000

    ノート取得・更新・削除APIの追加

---
+++ app/api/notes/[id]/route.ts
@@ -0,0 +1,31 @@
+import { zUpsertNote } from "@/app/notes/type";
+import { prisma } from "@/globals/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(_req: NextRequest, { params }: { params: { id: string } }) {
+  const note = await prisma.note.findUnique({
+    where: { id: Number(params.id) },
+  });
+  if (note === null) {
+    return new NextResponse(null, { status: 404 })
+  }
+  return NextResponse.json(note)
+}
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.update({
+    where: { id: Number(params.id) },
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+  return new NextResponse(null, { status: 204 })
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  const note = await prisma.note.delete({
+    where: { id: Number(params.id) },
+  });
+  return new NextResponse(null, { status: 204 })
+}
+
