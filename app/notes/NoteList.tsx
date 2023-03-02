97e0a6044631062a20b778b35aa42b34d7d965b5
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2023-03-02 04:16:31 +0000

    詳細ページへのリンクを追加

--- app/notes/NoteList.tsx
+++ app/notes/NoteList.tsx
@@ -36,7 +36,7 @@ const NoteItem: React.FC<NoteProps> = ({ item }) => {
           </svg>
         </span>
       </Link>
-      <Link href={`/notes`} prefetch={false}>
+      <Link href={`/notes/${item.id}`} prefetch={false}>
         <h3 className="text-pink-500 hover:text-pink-700 text-lg md:text-xl font-semibold mb-3 break-all underline underline-offset-2">{item.title}</h3>
       </Link>
       <p className="text-gray-500 break-all">{item.body}</p>
